db28b21bb1e2bc523d02a1a51be13557acaf8ea7
Maria_Veremey <Maria_Veremey@epam.com>
2020-08-21 00:53:41 +0300

    add mobile styles

--- movies/src/components/filter/ResultFilter.js
+++ movies/src/components/filter/ResultFilter.js
@@ -5,8 +5,12 @@ const genre = ['all', 'documentary', 'comedy', 'horror', 'crime'];
 export default function ResultFilter() {
   return (
     <ul className="genre-filter">
-      { genre.map((elem) => (
-        <li className="genre-filter__item">{elem}</li>
+      { genre.map((elem, index) => (
+        <li
+          className={!index ? 'genre-filter__item active' : 'genre-filter__item'}
+        >
+          {elem}
+        </li>
       ))}
     </ul>
   );
