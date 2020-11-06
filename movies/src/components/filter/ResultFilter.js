6508baec1d21f2a3e3eb563b1e7cbed0b408756f
Maria_Veremey <Maria_Veremey@epam.com>
2020-11-06 14:32:14 +0300

    Fix Sorting by genre

--- movies/src/components/filter/ResultFilter.js
+++ movies/src/components/filter/ResultFilter.js
@@ -3,17 +3,20 @@ import PropTypes from 'prop-types';
 import {connect} from "react-redux";
 import Constants from '../constants';
 import {fetchMovies} from "../../store/actions/actions";
+import { useHistory } from "react-router-dom";
 
 const matchDispatchToProps = {updateMovies: fetchMovies};
 
 const ResultFilter = (props) => {
   const { updateMovies } = props;
   const [activeGenre, setActiveGenre] = useState(0);
+  let history = useHistory();
 
   const filterMovies = useCallback((e, index) => {
     setActiveGenre(index);
     const genre = e.target.dataset && e.target.dataset.name;
     const currentGenre = (genre  === 'all') ? '' : genre;
+    history.push(`/search?filter=${currentGenre}`);
     updateMovies('', currentGenre);
   }, [updateMovies]);
 
