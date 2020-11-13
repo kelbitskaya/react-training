2bc0a8b98bb2e19c1010409b9728821bc4f2dc77
Maria_Veremey <Maria_Veremey@epam.com>
2020-11-13 21:40:42 +0300

    change folder structure

--- movies/with-redux-app/components/noMovie/NoMovie.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function NoMovie() {
-  return (
-    <div className="error-title__wrap">
-      <h1 className="error-title">No Movie Found</h1>
-    </div>
-  )
-};
