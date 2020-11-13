28d2d4b43bf611498cf14a0d87f61a04c4f52630
Maria_Veremey <Maria_Veremey@epam.com>
2020-11-13 22:28:33 +0300

    add styles

--- with-redux-app/pages/_app.js
+++ with-redux-app/pages/_app.js
@@ -1,21 +1,7 @@
-// import React from 'react';
-// import HomePage from "../pages/HomePage";
-// import { Provider } from "react-redux";
-// import store from "../store/store";
-// import '../scss/main.scss';
-//
-//
-// export default function App() {
-//   return (
-//     <Provider store={store}>
-//       <HomePage/>
-//     </Provider>
-//   );
-// }
-
 import React from 'react'
 import App from 'next/app';
 import {wrapper} from '../store/store';
+import '../scss/main.scss';
 
 
 class MyApp extends App {
